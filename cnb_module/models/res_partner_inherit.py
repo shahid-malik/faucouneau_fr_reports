9d044744b6f69e19970b4b3c6996632c32a11cfd
mehmoods <s.mehmood@landbellgroup.com>
2023-01-31 13:12:19 +0100

    refactoring

--- cnb_module/models/res_partner_inherit.py
+++ cnb_module/models/res_partner_inherit.py
@@ -4,6 +4,9 @@ from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
+    """
+    Customize Partner field to add custom fields as per needs of www.faucouneau.fr
+    """
     _inherit = 'res.partner'
 
     fax = fields.Char(string='Fax')
