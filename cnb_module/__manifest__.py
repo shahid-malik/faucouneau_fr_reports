04ca0ab45ebf56f26351e1c4bb87ebaca7a0351e
mehmoods <s.mehmood@landbellgroup.com>
2023-01-31 13:16:15 +0100

    update manifest file

--- cnb_module/__manifest__.py
+++ cnb_module/__manifest__.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 {
     "name": "cnb_module",
-    "version": "15.0.0.0",
-    "category": "Projects",
+    "version": "1.0",
+    "category": "Sale",
     "author": "Mediod Consulting",
-    "price": 50.00,
+    "price": 250.00,
     'currency': "USD",
-    'summary': '',
+    'summary': """
+        This module generates a lawyer contract for customers based on the data in customer model and law firm.
+        for faucouneau.fr law firm is the only company and data  company from company model.
+        New fields are added in customer model and company model to meet the criteria and insert into the customer
+        contract.
+    """,
     "website": "https://mediodconsulting.com/",
     "data": [
-        # 'data/custom_paperformat.xml',
         'report/cnb_paperformat.xml',
         'report/report_action.xml',
         'report/sales_report_inherit.xml',
