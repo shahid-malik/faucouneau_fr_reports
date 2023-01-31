ce5d61ce9fb140a2e9dc7f52b9043e38fa6e4df7
mehmoods <s.mehmood@landbellgroup.com>
2023-01-31 22:57:13 +0100

    add all fields in customer

--- cnb_module/models/res_partner_inherit.py
+++ cnb_module/models/res_partner_inherit.py
@@ -8,4 +8,8 @@ class ResPartner(models.Model):
 
     fax = fields.Char(string='Fax')
     dob = fields.Char(string='DoB')
+    control_gallery = fields.Char(string='DoB')
+    title = fields.Char(string='DoB')
+    mailing = fields.Char(string='DoB')
+    vip = fields.Char(string='DoB')
     nationality = fields.Many2one('res.country', 'Nationality',related='country_id' ,required=True)
