015b98362aec37e921a6da531f0c6ff0cd1d31ab
mehmoods <s.mehmood@landbellgroup.com>
2023-01-31 17:28:48 +0100

    updated report

--- cnb_module/models/res_partner_inherit.py
+++ cnb_module/models/res_partner_inherit.py
@@ -2,13 +2,9 @@
 
 from odoo import api, fields, models
 
-
 class ResPartner(models.Model):
-    """
-    Customize Partner field to add custom fields as per needs of www.faucouneau.fr
-    """
     _inherit = 'res.partner'
 
     fax = fields.Char(string='Fax')
     dob = fields.Char(string='DoB')
-    nationality = fields.Char(string='Nationality')
+    nationality = fields.Many2one('res.country', 'Nationality',related='country_id' ,required=True)
